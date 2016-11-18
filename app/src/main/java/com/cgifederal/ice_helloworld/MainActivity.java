ba75b6ba4a2b848cd71f5f81df4b3d52abbfef72
Christian Manalansan <christian.manalansan@cgifederal.com>
2016-11-18 14:34:09 -0500

    Implement a LocationListener. Check if the user gave the application the appropriate permissions by creating the Location request and display their the current location on the screen.

--- app/src/main/java/com/cgifederal/ice_helloworld/MainActivity.java
+++ app/src/main/java/com/cgifederal/ice_helloworld/MainActivity.java
@@ -1,18 +1,25 @@
 package com.cgifederal.ice_helloworld;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Point;
+import android.location.Location;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 import com.parse.FindCallback;
 import com.parse.GetCallback;
@@ -34,13 +41,16 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends FragmentActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
 
     public ParseObject PointOfInterest;
     public GoogleMap mMap;
+    private TextView textLat, textLong;
     // variables needed for GoogleApiClient
     private static final String TAG = MainActivity.class.getSimpleName();
     private GoogleApiClient googleApiClient;
+    private Location lastLocation;
+
     //List<PointOfInterest> parseObjects = new ArrayList<PointOfInterest>();
 
     @Override
@@ -87,6 +97,8 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
             }
         });
 
+        textLat = (TextView) findViewById(R.id.latValue);
+        textLong = (TextView) findViewById(R.id.lonValue);
         // Create GoogleApiClient
         createGoogleApi();
     }
@@ -123,6 +135,110 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onConnected(@Nullable Bundle bundle) {
         Log.i(TAG, "onConnected()");
+        getLastKnownLocation();
+    }
+
+    // Get last known location
+    private void getLastKnownLocation() {
+        Log.d(TAG, "getLastKnownLocation()");
+        if ( checkPermission() ) {
+            lastLocation = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
+            if ( lastLocation != null ) {
+                Log.i(TAG, "LasKnown location. " +
+                        "Long: " + lastLocation.getLongitude() +
+                        " | Lat: " + lastLocation.getLatitude());
+                writeLastLocation();
+                startLocationUpdates();
+            } else {
+                Log.w(TAG, "No location retrieved yet");
+                startLocationUpdates();
+            }
+        }
+        else askPermission();
+    }
+
+    private LocationRequest locationRequest;
+    // Defined in mili seconds.
+    // This number in extremely low, and should be used only for debug
+    private final int UPDATE_INTERVAL =  1000;
+    private final int FASTEST_INTERVAL = 900;
+
+    // Start location Updates
+    private void startLocationUpdates(){
+        Log.i(TAG, "startLocationUpdates()");
+        locationRequest = LocationRequest.create()
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                .setInterval(UPDATE_INTERVAL)
+                .setFastestInterval(FASTEST_INTERVAL);
+
+        if ( checkPermission() )
+            LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient, locationRequest, this);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Log.d(TAG, "onLocationChanged ["+location+"]");
+        lastLocation = location;
+        writeActualLocation(location);
+    }
+
+    // Write location coordinates on UI
+    private void writeActualLocation(Location location) {
+        textLat.setText( "Lat: " + location.getLatitude() );
+        textLong.setText( "Long: " + location.getLongitude() );
+    }
+
+    private void writeLastLocation() {
+        writeActualLocation(lastLocation);
+    }
+
+    // Check for permission to access Location
+    private boolean checkPermission() {
+        Log.d(TAG, "checkPermission()");
+        // Ask for permission if it wasn't granted yet
+        return (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED );
+    }
+
+    // Note from Christian: not sure if defining a constant in this fashion is good practice.
+    // If not, TODO: refactor how this constant is defined.
+    // Otherwise, TODO: delete this commentary
+    final private int REQ_PERMISSION = 123;
+
+    // Asks for permission
+    private void askPermission() {
+        Log.d(TAG, "askPermission()");
+        ActivityCompat.requestPermissions(
+                this,
+                new String[] { android.Manifest.permission.ACCESS_FINE_LOCATION },
+                REQ_PERMISSION
+        );
+    }
+
+    // Verify user's response of the permission requested
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        Log.d(TAG, "onRequestPermissionsResult()");
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch ( requestCode ) {
+            case REQ_PERMISSION: {
+                if ( grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED ){
+                    // Permission granted
+                    getLastKnownLocation();
+
+                } else {
+                    // Permission denied
+                    permissionsDenied();
+                }
+                break;
+            }
+        }
+    }
+
+    // App cannot work without the permissions
+    private void permissionsDenied() {
+        Log.w(TAG, "permissionsDenied()");
     }
 
     // GoogleApiClient.ConnectionCallbacks suspended
