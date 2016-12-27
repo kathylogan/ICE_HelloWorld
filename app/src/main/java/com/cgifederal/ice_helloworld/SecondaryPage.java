3f39c4f905f588bef233e805ffe66f66e522f918
Christian Manalansan <christian.manalansan@cgifederal.com>
2016-12-27 06:51:24 -0500

    By: Evan Wilkinson
    Query for a Parse Object, update the view with its details

--- app/src/main/java/com/cgifederal/ice_helloworld/SecondaryPage.java
+++ app/src/main/java/com/cgifederal/ice_helloworld/SecondaryPage.java
@@ -11,12 +11,15 @@ import android.widget.Toast;
 import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.SaveCallback;
+import com.parse.ParseQuery;
+import com.parse.GetCallback;
 
 /**
  * Created by Ayush on 8/12/2016.
  */
 public class SecondaryPage extends Activity {
-    ParseObject testObject;
+    PointOfInterest testObject;
+    PointOfInterest replyObject;
 
     private static final String NOTIFICATION_MSG = "NOTIFICATION MSG";
     // Create a Intent send by the notification
@@ -31,19 +34,55 @@ public class SecondaryPage extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.secondary_page);
 
+        testObject = new PointOfInterest();
+        replyObject = new PointOfInterest();
+        testObject.put("Feature", "Test Feature Name");
+        testObject.setFeature("The Feature Name");
+        testObject.put("pointOfInterestName", "Point of Interest Name");
+        testObject.put("eventBody", "Test Event Body");
+
+        String objectId = testObject.getID();
+
+        ParseQuery<PointOfInterest> query = new ParseQuery("PointOfInterest");
+         query.getInBackground(objectId, new GetCallback<PointOfInterest>() {
+             public void done(PointOfInterest object, ParseException e) {
+                 if (e == null) {
+                     replyObject.put("Feature", object.getFeature());
+                     replyObject.setFeature(object.getFeature());
+                     updateObject(replyObject);
+                 } else {
+                     //NULL;
+                 }
+             }
+         });
+
         Intent intent = getIntent();
         String pointOfInterestId = intent.getStringExtra(Intent.EXTRA_TEXT);
         // set data based on Parse record
-        TextView featureName = (TextView) findViewById(R.id.featureName);
-        featureName.setText("Brownfield");
-        TextView pointOfInterestName = (TextView) findViewById(R.id.pointOfInterestName);
-        pointOfInterestName.setText("Berry Lane Property 5");
-        TextView eventBody = (TextView) findViewById(R.id.eventBody);
-        eventBody.setText("EPA has selected the Jersey City Redevelopment Agency for three brownfields cleanup grants. Hazardous substances...");
 
-
-        testObject = new ParseObject("TestObject");
     }
+
+    protected void updateObject(PointOfInterest object) {
+        setContentView(R.layout.secondary_page);
+        String fName = replyObject.getFeature();
+        String pName = object.getString("pointOfInterestName");
+        String eBody = object.getString("eventBody");
+
+        TextView featureName = (TextView) findViewById(R.id.featureName);
+        //featureName.setText("Brownfield");
+        featureName.setText(fName);
+        TextView pointOfInterestName = (TextView) findViewById(R.id.pointOfInterestName);
+        //pointOfInterestName.setText("Berry Lane Property 5");
+        pointOfInterestName.setText(replyObject.getString("pointOfInterestName"));
+        TextView eventBody = (TextView) findViewById(R.id.eventBody);
+        //eventBody.setText("EPA has selected the Jersey City Redevelopment Agency for three brownfields cleanup grants. Hazardous substances...");
+        eventBody.setText(eBody);
+
+        //testObject = new ParseObject("TestObject");
+    }
+
+
+     
 
     /**
      * Click handler for "Push and Check" button
