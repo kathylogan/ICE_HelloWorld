8246768d2e115299bdc8c1649dda6fcbb70094a8
Christian Manalansan <christian.manalansan@cgifederal.com>
2016-12-27 11:19:05 -0500

    Override GeoFenceDialogFragment constructor to accept a point of interest

--- app/src/main/java/com/cgifederal/ice_helloworld/GeoFenceDialogFragment.java
+++ app/src/main/java/com/cgifederal/ice_helloworld/GeoFenceDialogFragment.java
@@ -7,7 +7,19 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.parse.ParseObject;
+
 public class GeoFenceDialogFragment extends DialogFragment {
+    String pointOfInterestId;
+
+
+    public GeoFenceDialogFragment() {
+    }
+    // TODO: avoid non-default constructors
+    public GeoFenceDialogFragment(ParseObject poi) {
+        pointOfInterestId = poi.getObjectId();
+    }
+
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
@@ -16,7 +28,7 @@ public class GeoFenceDialogFragment extends DialogFragment {
                 .setPositiveButton("Continue", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
                         Intent intent = new Intent(getActivity(), SecondaryPage.class);
-                        intent.putExtra(Intent.EXTRA_TEXT, "xQorpb2bSu");
+                        intent.putExtra(Intent.EXTRA_TEXT, pointOfInterestId);
                         startActivity(intent);
                     }
                 });
